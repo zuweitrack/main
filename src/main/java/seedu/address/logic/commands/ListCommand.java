3c851133d4259c7721a35090cfe3b87577a4895d
fuadsahmawi <fuadsahmawi@gmail.com>
2018-03-09 23:43:45 +0800

    Revert "Added new"
    This reverts commit 588f32317d92c0262a3b4a7121055c33b1601cbc.

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -9,8 +9,6 @@ public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
-    public static final String COMMAND_ALIAS = "l";
-
     public static final String MESSAGE_SUCCESS = "Listed all persons";
 
 
