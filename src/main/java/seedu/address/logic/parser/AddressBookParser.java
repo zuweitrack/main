f7589995bf06102fb80334dbc658f335a9887829
fuadsahmawi <fuadsahmawi@gmail.com>
2018-03-09 23:40:56 +0800

    Added "find" command alias: "f"

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -81,6 +81,9 @@ public class AddressBookParser {
         case FindCommand.COMMAND_WORD:
             return new FindCommandParser().parse(arguments);
 
+        case FindCommand.COMMAND_ALIAS:
+            return new FindCommandParser().parse(arguments);
+
         case ListCommand.COMMAND_WORD:
             return new ListCommand();
 
