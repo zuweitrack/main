c8877662fa00ec1523a227ab474ba88cb4eaa6d1
fuadsahmawi <fuadsahmawi@gmail.com>
2018-03-15 11:45:37 +0800

    Find tags now working, tests still not done yet

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -5,18 +5,14 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 import static seedu.address.logic.parser.CliSyntax.*;
 
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.Optional;
-import java.util.Set;
+import java.util.stream.Stream;
 
-import seedu.address.commons.core.index.Index;
-import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.TagContainsKeywordsPredicate;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
-import seedu.address.model.tag.Tag;
+
 
 /**
  * Parses input arguments and creates a new FindCommand object
@@ -34,6 +30,11 @@ public class FindCommandParser implements Parser<FindCommand> {
         requireNonNull(args);
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_TAG);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_TAG)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        }
        
         name = argMultimap.getValue(PREFIX_NAME);
         tag = argMultimap.getValue(PREFIX_TAG);
@@ -41,22 +42,16 @@ public class FindCommandParser implements Parser<FindCommand> {
         String[] nameKeywords = name.get().split("\\s+");
         String[] tagKeywords = tag.get().split("\\s+");
         
-        if (tag.get().equals(""))
-        return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
-        if (name.get().equals("")) 
-            return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(tagKeywords)));
-        
-        /*String trimmedArgs = args.trim();
-        if (trimmedArgs.isEmpty()) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        if (tag.get().equals("")) {
+            return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        } else {
+            return new FindCommand(new TagContainsKeywordsPredicate(Arrays.asList(tagKeywords)));
         }
-
-        String[] nameKeywords = trimmedArgs.split("\\s+");
-
-        return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
-        */
-        return null;
+        
+        
     }
-
+    
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
 }
