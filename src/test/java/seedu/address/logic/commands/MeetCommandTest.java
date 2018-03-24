2c52c23798369f8125360ad2dd89ece2d91a29cf
A0158738X <Sameenhaja@Sameens-MacBook-Pro.local>
2018-03-25 03:09:27 +0800

    Checkstyle import issue

--- src/test/java/seedu/address/logic/commands/MeetCommandTest.java
+++ src/test/java/seedu/address/logic/commands/MeetCommandTest.java
@@ -3,7 +3,6 @@ package seedu.address.logic.commands;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_UNUSED;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_MEETDATE_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
