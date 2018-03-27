f4b3e8d11be7bae67876c21ea1cef61f405cec18
zuweitrack <e0105028@u.nus.edu>
2018-03-27 18:45:31 +0800

    Add string "RA" to search for RAs in SeekRaCommandParser

--- src/main/java/seedu/address/logic/parser/SeekRaCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SeekRaCommandParser.java
@@ -19,13 +19,16 @@ public class SeekRaCommandParser implements Parser<SeekRaCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public SeekRaCommand parse(String args) throws ParseException {
+
         String trimmedArgs = args.trim();
         if (trimmedArgs.isEmpty()) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, SeekRaCommand.MESSAGE_USAGE));
         }
 
-        String[] nameKeywords = trimmedArgs.split("\\s+");
+        trimmedArgs = trimmedArgs + " " + "RA";
+
+        String[] nameKeywords = (trimmedArgs.split("\\s+"));
 
         return new SeekRaCommand(new UnitNumberContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
     }
