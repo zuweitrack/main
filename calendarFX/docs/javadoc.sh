130cadd2a54eabc97a75be01b57d1d5dbcbf5477
fuadsahmawi <fuadsahmawi@gmail.com>
2018-03-29 13:37:54 +0800

    Add calendarFX api

---
+++ calendarFX/docs/javadoc.sh
@@ -0,0 +1,1 @@
+/Library/Java/JavaVirtualMachines/jdk1.8.0_122.jdk/Contents/Home/jre/../bin/javadoc -J-Djavafx.javadoc=true @options @packages
--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -1,14 +1,21 @@
 package seedu.address;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Map;
 import java.util.Optional;
 import java.util.logging.Logger;
 
+import com.calendarfx.model.Calendar;
+import com.calendarfx.model.Calendar.Style;
+import com.calendarfx.model.CalendarSource;
+import com.calendarfx.view.CalendarView;
 import com.google.common.eventbus.Subscribe;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import seedu.address.commons.core.Config;
 import seedu.address.commons.core.EventsCenter;
@@ -185,7 +192,52 @@ public class MainApp extends Application {
     @Override
     public void start(Stage primaryStage) {
         logger.info("Starting AddressBook " + MainApp.VERSION);
-        ui.start(primaryStage);
+        CalendarView calendarView = new CalendarView();
+
+        Calendar birthdays = new Calendar("Birthdays");
+        Calendar holidays = new Calendar("Holidays");
+
+        birthdays.setStyle(Style.STYLE1);
+        holidays.setStyle(Style.STYLE2);
+
+        CalendarSource myCalendarSource = new CalendarSource("My Calendars");
+        myCalendarSource.getCalendars().addAll(birthdays, holidays);
+
+        calendarView.getCalendarSources().addAll(myCalendarSource);
+
+        calendarView.setRequestedTime(LocalTime.now());
+
+        Thread updateTimeThread = new Thread("Calendar: Update Time Thread") {
+            @Override
+            public void run() {
+                while (true) {
+                    Platform.runLater(() -> {
+                        calendarView.setToday(LocalDate.now());
+                        calendarView.setTime(LocalTime.now());
+                    });
+
+                    try {
+                        // update every 10 seconds
+                        sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            };
+        };
+
+        updateTimeThread.setPriority(Thread.MIN_PRIORITY);
+        updateTimeThread.setDaemon(true);
+        updateTimeThread.start();
+
+        Scene scene = new Scene(calendarView);
+        primaryStage.setTitle("Calendar");
+        primaryStage.setScene(scene);
+        primaryStage.setWidth(1300);
+        primaryStage.setHeight(1000);
+        primaryStage.centerOnScreen();
+        primaryStage.show();
     }
 
     @Override
