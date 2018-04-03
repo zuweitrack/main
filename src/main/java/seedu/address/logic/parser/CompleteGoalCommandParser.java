55b1adbb92e305736e813d7c4383bc1335ba8bd1
deborahlow97 <deborahlow97@gmail.com>
2018-04-03 16:29:47 +0800

    Add partial complete commands into code

---
+++ src/main/java/seedu/address/logic/parser/CompleteGoalCommandParser.java
@@ -0,0 +1,31 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.CompleteCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new CompleteCommand object
+ */
+public class CompleteCommandParser implements Parser<CompleteCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the CompleteCommand
+     * and returns an CompleteCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public CompleteCommand parse(String args) throws ParseException {
+        try {
+            Index index = ParserUtil.parseIndex(args);
+            return new CompleteCommand(index);
+        } catch (IllegalValueException ive) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, CompleteCommand.MESSAGE_USAGE));
+        }
+    }
+
+}
+
