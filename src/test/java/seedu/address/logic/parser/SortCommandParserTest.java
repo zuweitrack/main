b2af3dddfcbfa3372f69800d592638983a217c64
sham-sheer <Sameenhaja@Sameens-MacBook-Pro-3.local>
2018-04-12 02:40:05 +0800

    Added Collate

--- src/test/java/seedu/address/logic/parser/SortCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/SortCommandParserTest.java
@@ -2,8 +2,6 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static seedu.address.testutil.TypicalIndexes.INDEX_SORT_LEVEL_OF_FRIENDSHIP;
 
 import org.junit.Test;
 
@@ -14,7 +12,6 @@ import seedu.address.logic.commands.SortCommand;
 //@@author sham-sheer
 public class SortCommandParserTest {
     private SortCommandParser parser = new SortCommandParser();
-    
     @Test
     public void parse_invalidArgs_throwsParseException() {
         assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
