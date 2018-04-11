3750491041b81372c8784ea7cfbcb26dbea1e188
fuadsahmawi <fuadsahmawi@gmail.com>
2018-04-11 13:01:44 +0800

    Fixed checkstyle error

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -11,7 +11,8 @@ public class Messages {
             "Start Date cannot be later than End Date or Start/End Date cannot be earlier than current date! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
     public static final String MESSAGE_INVALID_GOAL_DISPLAYED_INDEX = "The goal index provided is invalid";
-    public static final String MESSAGE_INVALID_REMINDER_TEXT_DATE = "The reminder text or start date provided is invalid";
+    public static final String MESSAGE_INVALID_REMINDER_TEXT_DATE = "The reminder text or start date "
+            + "provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_RA_LISTED_OVERVIEW = "%1$d Searched Persons and "
             + "their Resident Assistant(s) (RA) listed!";
