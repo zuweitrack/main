766655fa5c0f430ed287bcf444a4c1b4c5b94d4f
zuweitrack <e0105028@u.nus.edu>
2018-04-13 22:17:50 +0800

    Edit RateCommand to handle invalid entries

--- src/main/java/seedu/address/logic/parser/RateCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RateCommandParser.java
@@ -6,6 +6,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_LEVEL_OF_FRIENDSHIP;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
@@ -19,6 +20,20 @@ import seedu.address.logic.parser.exceptions.ParseException;
 public class RateCommandParser {
 
     /**
+     * Returns true if the level of friendship prefix "/*" is present
+     */
+    private static boolean isPrefixesPresent(ArgumentMultimap argumentMultimap, Prefix prefix) {
+        return Stream.of(prefix).allMatch(groupPrefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+
+    /**
+     * Returns true if the level of friendship is between 1 - 10
+     */
+    private static boolean containsValidRange(String levelOfFriendship) {
+        return levelOfFriendship.matches("0?[1-9]|[1][0]");
+    }
+
+    /**
      * Parses the given {@code String} of arguments in the context of the RateCommand
      * and returns an RateCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
@@ -26,6 +41,10 @@ public class RateCommandParser {
     public RateCommand parse (String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argumentMultimap = ArgumentTokenizer.tokenize(args, PREFIX_LEVEL_OF_FRIENDSHIP);
+
+        if (!isPrefixesPresent(argumentMultimap, PREFIX_LEVEL_OF_FRIENDSHIP)) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, RateCommand.MESSAGE_USAGE));
+        }
 
         String preamble;
         String[] indexString;
@@ -45,6 +64,10 @@ public class RateCommandParser {
 
 
         String levelOfFriendship = argumentMultimap.getValue(PREFIX_LEVEL_OF_FRIENDSHIP).get();
+
+        if (!containsValidRange(levelOfFriendship)) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, RateCommand.MESSAGE_USAGE));
+        }
 
         return new RateCommand(indexList, new String(levelOfFriendship));
     }
