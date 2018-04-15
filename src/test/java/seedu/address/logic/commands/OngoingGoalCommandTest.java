7c841141e0c86b052490e6922e599e3e727c8c2b
deborahlow97 <deborahlow97@gmail.com>
2018-04-15 18:14:24 +0800

    Update DG and add tests

--- src/test/java/seedu/address/logic/commands/OngoingGoalCommandTest.java
+++ src/test/java/seedu/address/logic/commands/OngoingGoalCommandTest.java
@@ -3,12 +3,9 @@ package seedu.address.logic.commands;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.GoalCommandTestUtil.DESC_GOAL_COMPLETED_E;
-import static seedu.address.logic.commands.GoalCommandTestUtil.DESC_GOAL_COMPLETED_F;
-import static seedu.address.logic.commands.GoalCommandTestUtil.VALID_GOAL_COMPLETION_D;
 import static seedu.address.logic.commands.GoalCommandTestUtil.VALID_GOAL_COMPLETION_E;
 import static seedu.address.logic.commands.GoalCommandTestUtil.VALID_GOAL_END_DATE_TIME;
 import static seedu.address.logic.commands.GoalCommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.GoalCommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.GoalCommandTestUtil.prepareRedoCommand;
 import static seedu.address.logic.commands.GoalCommandTestUtil.prepareUndoCommand;
 import static seedu.address.testutil.TypicalGoals.getTypicalGoalAddressBook;
@@ -38,26 +35,6 @@ import seedu.address.testutil.OngoingGoalDescriptorBuilder;
 public class OngoingGoalCommandTest {
 
     private Model model = new ModelManager(getTypicalGoalAddressBook(), new UserPrefs());
-
-    @Test
-    public void execute_goalIsCompletedUnfilteredList_success() throws Exception {
-        Index indexLastGoal = Index.fromOneBased(model.getFilteredGoalList().size());
-        Goal lastGoal = model.getFilteredGoalList().get(indexLastGoal.getZeroBased());
-
-        GoalBuilder goalInList = new GoalBuilder(lastGoal);
-        Goal completedGoal = goalInList.withCompletion(VALID_GOAL_COMPLETION_D).build();
-
-        OngoingGoalDescriptor descriptor = new OngoingGoalDescriptorBuilder().withCompletion(VALID_GOAL_COMPLETION_E)
-                .withEndDateTime(VALID_GOAL_END_DATE_TIME).build();
-        OngoingGoalCommand ongoingGoalCommand = prepareCommand(indexLastGoal, descriptor);
-
-        String expectedMessage = String.format(CompleteGoalCommand.MESSAGE_COMPLETE_GOAL_SUCCESS, completedGoal);
-
-        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
-        expectedModel.updateGoalWithoutParameters(lastGoal, completedGoal);
-
-        assertCommandSuccess(ongoingGoalCommand, model, expectedMessage, expectedModel);
-    }
 
     @Test
     public void execute_goalAlreadyOngoingUnfilteredList_throwsCommandException() throws Exception {
@@ -91,29 +68,6 @@ public class OngoingGoalCommandTest {
     }
 
     @Test
-    public void executeUndoRedo_validIndexUnfilteredList_success() throws Exception {
-        UndoRedoStack undoRedoStack = new UndoRedoStack();
-        UndoCommand undoCommand = prepareUndoCommand(model, undoRedoStack);
-        RedoCommand redoCommand = prepareRedoCommand(model, undoRedoStack);
-        Goal ongoingGoal = new GoalBuilder(VALID_GOAL_COMPLETION_D).build();
-        Goal goalToEdit = model.getFilteredGoalList().get(INDEX_FIRST_GOAL.getZeroBased());
-        OngoingGoalDescriptor descriptor = new OngoingGoalDescriptorBuilder(ongoingGoal).build();
-        OngoingGoalCommand ongoingGoalCommand = prepareCommand(INDEX_FIRST_GOAL, descriptor);
-        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
-
-        // edit -> first goal ongoing
-        ongoingGoalCommand.execute();
-        undoRedoStack.push(ongoingGoalCommand);
-
-        // undo -> reverts addressbook back to previous state and filtered goal list to show all goals
-        assertCommandSuccess(undoCommand, model, UndoCommand.MESSAGE_SUCCESS, expectedModel);
-
-        // redo -> same first goal ongoing again
-        expectedModel.updateGoalWithoutParameters(goalToEdit, ongoingGoal);
-        assertCommandSuccess(redoCommand, model, RedoCommand.MESSAGE_SUCCESS, expectedModel);
-    }
-
-    @Test
     public void executeUndoRedo_invalidIndexUnfilteredList_failure() {
         UndoRedoStack undoRedoStack = new UndoRedoStack();
         UndoCommand undoCommand = prepareUndoCommand(model, undoRedoStack);
@@ -143,10 +97,6 @@ public class OngoingGoalCommandTest {
         // same object -> returns true
         assertTrue(standardCommand.equals(standardCommand));
 
-        // one command preprocessed when previously equal -> returns false
-        commandWithSameValues.preprocessUndoableCommand();
-        assertFalse(standardCommand.equals(commandWithSameValues));
-
         // null -> returns false
         assertFalse(standardCommand.equals(null));
 
@@ -155,9 +105,6 @@ public class OngoingGoalCommandTest {
 
         // different index -> returns false
         assertFalse(standardCommand.equals(new OngoingGoalCommand(INDEX_SECOND_GOAL, DESC_GOAL_COMPLETED_E)));
-
-        // different descriptor -> returns false
-        assertFalse(standardCommand.equals(new OngoingGoalCommand(INDEX_FIRST_GOAL, DESC_GOAL_COMPLETED_F)));
     }
 
     /**
