8d5a088312fb7db1b737af120c2f3afdf94753c8
deborahlow97 <deborahlow97@gmail.com>
2018-04-15 21:00:10 +0800

    Fixed birthday bug

--- src/main/java/seedu/address/model/person/Birthday.java
+++ src/main/java/seedu/address/model/person/Birthday.java
@@ -2,7 +2,6 @@ package seedu.address.model.person;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
-import static seedu.address.logic.parser.DateTimeParser.containsDateAndTime;
 import static seedu.address.logic.parser.DateTimeParser.nattyDateAndTimeParser;
 
 import java.time.LocalDateTime;
@@ -14,7 +13,8 @@ import java.time.LocalDateTime;
  */
 public class Birthday {
 
-    public static final String MESSAGE_BIRTHDAY_CONSTRAINTS = "Person birthday should be a valid date.";
+    public static final String MESSAGE_BIRTHDAY_CONSTRAINTS = "Person birthday should be a valid date and cannot "
+            + "be later than today's date.";
     public static final String BIRTHDAY_VALIDATION_REGEX =
             "^(((0[1-9]|[12]\\d|3[01])\\/(0[13578]|1[02])\\/((19|[2-9]\\d)\\d{2}))|((0[1-9]|[12]\\d|30)"
                     + "\\/(0[13456789]|1[012])\\/((19|[2-9]\\d)\\d{2}))|((0[1-9]|1\\d|2[0-8])\\/02\\/((19|[2-9]\\d)"
@@ -30,26 +30,18 @@ public class Birthday {
      */
     public Birthday(String birthday) {
         requireNonNull(birthday);
-        checkArgument(isValidBirthdayRegex(birthday), MESSAGE_BIRTHDAY_CONSTRAINTS);
         checkArgument(isValidBirthday(birthday), MESSAGE_BIRTHDAY_CONSTRAINTS);
         this.value = birthday;
-    }
-
-    /**
-     * Returns if a given string is a valid person birthday.
-     */
-    public static boolean isValidBirthdayRegex(String test) {
-        return test.matches(BIRTHDAY_VALIDATION_REGEX);
     }
 
     /**
      * Returns if a given string is a valid person birthday (before current date).
      */
     public static boolean isValidBirthday(String test) {
-        LocalDateTime birthdayLocalDateTime;
+        LocalDateTime birthdayLocalDateTime = null;
         LocalDateTime currentLocalDateTime = LocalDateTime.now();
-        String birthdayInDifferentFormat = getDifferentBirthdayFormat(test);
-        if (containsDateAndTime(birthdayInDifferentFormat)) {
+        if (test.matches(BIRTHDAY_VALIDATION_REGEX)) {
+            String birthdayInDifferentFormat = getDifferentBirthdayFormat(test);
             birthdayLocalDateTime = nattyDateAndTimeParser(birthdayInDifferentFormat).get();
         } else {
             return false;
